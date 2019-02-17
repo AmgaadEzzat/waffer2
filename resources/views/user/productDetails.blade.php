7434a98263c96e3202edb2dc6b561d09fc7904d9
ireenAnwar <ireen.bolos@gmail.com>
2019-02-17 23:44:55 +0200

    changing in product details style

--- resources/views/user/productDetails.blade.php
+++ resources/views/user/productDetails.blade.php
@@ -2,8 +2,8 @@
 @section('content')
 
     <div class="container-fluid">
-        <div class="row container-fluid">
-            <div class="col-sm-2 border  border-secondary ">
+        <div class="row container-fluid ">
+            <div class="col-sm-2 border shadow  border-secondary ">
                 <div class="dropdown">
                     <button type="button" class="btn  dropdown-toggle" data-toggle="dropdown">
                         Browse By Category
@@ -26,36 +26,13 @@
         </div>
     </div>
     <div class="container m-5 ">
-        <div class="row">
-            <div class="col-sm-3 col-xs-2 border  border-secondary">
+        <div class="row ">
+            <div class="col-sm-3 shadow rounded-right col-xs-2 border  border-secondary">
 
-                <a href="#" data-toggle="modal" data-target="#myModal"> <img src="images/mob1.jpg" class="w-100 m-2">  </a>
-                <div id="demo" class="carousel slide" data-ride="carousel">
-
+                <a href="#" data-toggle="modal" data-target="#myModal"> <img src="images/mob1.jpg" class="w-100 m-2  rounded">  </a>
 
 
-                    <!-- The slideshow -->
-                    <div class="carousel-inner m-2">
-                        <div class="carousel-item active">
-                            <img src="images/2.jpg" class="w-25 " >
-                        </div>
-                        <div class="carousel-item ">
-                            <img src="images/3.jpg"  class="w-25">
-                        </div>
-                        <div class="carousel-item ">
-                            <img src="images/huawei_nova3i.jpg"  class="w-25 ">
-                        </div>
-                    </div>
 
-                    <!-- Left and right controls -->
-                    <a class="carousel-control-prev" href="#demo" data-slide="prev">
-                        <span class="carousel-control-prev-icon"></span>
-                    </a>
-                    <a class="carousel-control-next" href="#demo" data-slide="next">
-                        <span class="carousel-control-next-icon"></span>
-                    </a>
-
-                </div>
                 <div class="modal fade" id="myModal">
                     <div class="modal-dialog modal-dialog-centered">
                         <div class="modal-content">
@@ -68,35 +45,7 @@
 
 
                             <div class="modal-body">
-                                <div id="modelDemo" class="carousel slide" data-ride="carousel">
-
-
-
-                                    <!-- The slideshow -->
-                                    <div class="carousel-inner m-2">
-                                        <div class="carousel-item active">
-                                            <img src="images/2.jpg" class="w-50 " >
-                                        </div>
-                                        <div class="carousel-item">
-                                            <img src="images/3.jpg"  class="w-50 ">
-                                        </div>
-                                        <div class="carousel-item">
-                                            <img src="images/4.jpg"  class="w-50 ">
-                                        </div>
-                                        <div class="carousel-item ">
-                                            <img src="images/huawei_nova3i.jpg"  class="w-50 ">
-                                        </div>
-                                    </div>
-
-                                    <!-- Left and right controls -->
-                                    <a class="carousel-control-prev" href="#modelDemo" data-slide="prev">
-                                        <span class="carousel-control-prev-icon"></span>
-                                    </a>
-                                    <a class="carousel-control-next" href="#modelDemo" data-slide="next">
-                                        <span class="carousel-control-next-icon"></span>
-                                    </a>
-
-                                </div>
+                                <img src="images/mob1.jpg" class="w-100 m-2">
 
                             </div>
 
@@ -106,27 +55,24 @@
 
 
 
-                    </div>
 
 
 
+
+
+            </div>
                 </div>
             </div>
 
 
-
-            <div class="col-sm-9  border  border-secondary border-left-0 container">  <a href="#">  <h1>Product name and details</h1></a>
-                <span class="border border-secondary w-75 p-2 m-4"  > Price 333.22$</span>
-                <span class="badge badge-success m-4"> live</span>
+            <div class="col-sm-8  rounded-left  border  border-secondary container">  <a href="#">  <h1 class="m-4">Product name and details</h1></a>
+                <span class="border border-success w-75 p-2 m-4"  > Price 333.22$</span>
+                <span class="badge badge-success m-2"> live</span>
                 <br>
                 <span class="m-4"> product details </span>
                 <div class="m-4"> <button class="btn btn-success"> Buy it from Jumia</button> </div>
                 <div id=stars class="m-4 d-inline-flex">
-                    <span style="color:rgb(179, 75, 75);"><i class="far fa-star"></i></span>
-                    <span style="color:rgb(179, 75, 75);"><i class="far fa-star"></i></span>
-                    <span style="color:rgb(179, 75, 75);"><i class="far fa-star"></i></span>
-                    <span style="color:rgb(179, 75, 75);"><i class="far fa-star"></i></span>
-                    <span style="color:rgb(179, 75, 75);"><i class="far fa-star"></i></span>
+
                     <ul class="nav d-inline-flex ">
                         <li class=""><a href="#" class="p-2"> Be the frist to review</a></li>
                         <li ><a href="#" class="p-2"> Add a wish list</a></li>
@@ -155,7 +101,7 @@
         <div class="row " id=bestsellerDiv>
 
             <div class="col-sm-3 " >
-                <div class="crad border ">
+                <div class="crad border shadow ">
                     <img src="images/2.jpg"  class="w-50 m-5">
                     <div class="card-body cardstyle" >
                         <a href="#"  class="m-4 text-center">  product name </a>
@@ -180,6 +126,8 @@
 
 
     </div>
-
+            </div>
+        </div>
+    </div>
 
 @stop
