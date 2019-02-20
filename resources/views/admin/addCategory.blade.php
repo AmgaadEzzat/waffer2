49bc4e0866c86faeb8d9137e2561628a6a09ad7e
AmgaadEzzat <amgaadezzat99@gmail.com>
2019-02-20 22:36:34 +0200

    add package socialite

--- resources/views/admin/addCategory.blade.php
+++ resources/views/admin/addCategory.blade.php
@@ -49,26 +49,41 @@
                 </div>
             </div>
         </div>
-        <div class="container pt-5">
-            <h2>All Categories</h2>
-         <table class="table table-bordered w-50 bg-white">
-          <thead>
-
-            <th style="color: #ff5370">Category Name </th>
-          </thead>
-             <tbody>
-             @foreach($categories as $category)
-              <tr>
-                  <td>
-                   {{$category->categoryName}}
-                  </td>
-                  <td><a href="/{{$category->id}}/deleteCategory"><i class="fas fa-trash" style="color: #17a2b8"></i></a></td>
-              </tr>
-                 @endforeach
-             </tbody>
-         </table>
+        {{--<div class="container pt-5">--}}
+            <div class="row">
+            <div class="col-md-8" style="font-size: 1.5em">
+            All Categories
+            </div>
+            </div>
+            <div class="row">
+             <div class="col-md-8">
+                 <table class="table table-bordered w-50 bg-white">
+                     <thead>
+                     <div class="row">
+                     <div class="col-md-6">
+                        <th style="color: #ff5370">Category Name </th>
+                     </div>
+                     </div>
+                     </thead>
+                     <tbody>
+                     <div class="row">
+                     <div class="col-md-6">
+                         @foreach($categories as $category)
+                          <tr>
+                              <td>
+                               {{$category->categoryName}}
+                              </td>
+                              <td><a href="/{{$category->id}}/deleteCategory"><i class="fas fa-trash" style="color: #17a2b8"></i></a></td>
+                          </tr>
+                             @endforeach
+                     </div>
+                     </div>
+                     </tbody>
+                 </table>
         </div>
-    </div>
+        </div>
+        </div>
+    {{--</div>--}}
 @endsection
 
 
--- resources/views/admin/master.blade.php
+++ resources/views/admin/master.blade.php
@@ -78,56 +78,56 @@
 
     <div id="content" class="container">
         <div class="row">
-        <div class="col-md-12">
-        <nav class="navbar navbar-expand-md navbar-light ">
-            <div class="container-fluid">
+            <div class="col-md-12">
+                <nav class="navbar navbar-expand-md navbar-light ">
+                    <div class="container-fluid">
 
-                <button type="button" id="sidebarCollapse" class="btn">
-                    <i class="fas fa-align-left"></i>
-                    <span>Toggle Sidebar</span>
-                </button>
+                        <button type="button" id="sidebarCollapse" class="btn">
+                            <i class="fas fa-align-left"></i>
+                            <span>Toggle Sidebar</span>
+                        </button>
 
 
-                    <ul class="nav navbar-nav ml-auto">
-                        @guest
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                            </li>
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
+                        <ul class="nav navbar-nav ml-auto">
+                            @guest
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                </li>
+                            @else
+                                <li class="nav-item dropdown">
+                                    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                        {{ Auth::user()->name }} <span class="caret"></span>
                                     </a>
 
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
+                                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                        <a class="dropdown-item" href="{{ route('logout') }}"
+                                           onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                            {{ __('Logout') }}
+                                        </a>
 
-        </nav>
+                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                            @csrf
+                                        </form>
+                                    </div>
+                                </li>
+                            @endguest
+                        </ul>
+                    </div>
+
+                </nav>
 
 
-        {{--Main Content--}}
-        @yield('content')
+                {{--Main Content--}}
+                @yield('content')
 
 
+            </div>
+        </div>
     </div>
-    </div>
-</div>
 </div>
 
 <script  src="{{asset('js/dashboard.js')}}"></script>
